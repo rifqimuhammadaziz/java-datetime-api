7dfcde4a90943ffadc3fa6a32ffa326e99072f75
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-02-07 17:06:35 +0700

    create test Date

--- src/main/java/rifqimuhammadaziz/datetime/App.java
+++ src/main/java/rifqimuhammadaziz/datetime/App.java
@@ -1,4 +1,4 @@
-package rifqimuhammadaziz.datetime-api;
+package rifqimuhammadaziz.datetime;
 
 /**
  * Hello world!
---
+++ src/test/java/rifqimuhammadaziz/datetime/DateTest.java
@@ -0,0 +1,19 @@
+package rifqimuhammadaziz.datetime;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+
+public class DateTest {
+
+    @Test
+    void create() {
+        var date1 = new Date();
+        var date2 = new Date(System.currentTimeMillis());
+        var date3 = new Date(1644228306504L);
+
+        System.out.println(date1);
+        System.out.println(date2);
+        System.out.println(date3);
+    }
+}
